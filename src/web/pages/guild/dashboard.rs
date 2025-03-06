e821e98195e8358dc4ffe001e18879ffb67894d2
ElementalAlchemist <ElementAlchemist7@gmail.com>
2025-03-05 23:45:02 -0600

    Show closed tickets on the dashboard

--- src/web/pages/guild/dashboard.rs
+++ src/web/pages/guild/dashboard.rs
@@ -15,21 +15,22 @@ pub fn Dashboard() -> impl IntoView {
 	let params = use_params::<GuildParam>();
 	let guild_id = params.read().as_ref().ok().and_then(|params| params.guild);
 
-	let user_tickets = Resource::new(|| (), move |_| get_active_tickets_for_user(guild_id));
+	let user_active_tickets = Resource::new(|| (), move |_| get_active_tickets_for_user(guild_id));
+	let user_closed_tickets = Resource::new(|| (), move |_| get_closed_tickets_for_user(guild_id));
 
 	view! {
 		<Transition fallback=|| view! { <div class="dashboard_ticket_list_loading">"Loading tickets..."</div> }>
 			<table class="dashboard_ticket_list">
 				<thead>
 					<tr>
-						<th>Ticket</th>
-						<th>Last Message Author</th>
-						<th>Last Message Time</th>
+						<th>"Ticket"</th>
+						<th>"Last Message Author"</th>
+						<th>"Last Message Time"</th>
 					</tr>
 				</thead>
 				<tbody>
 					{
-						move || match &user_tickets.read().as_ref().and_then(|tickets| tickets.as_ref().ok()) {
+						move || match &user_active_tickets.read().as_ref().and_then(|tickets| tickets.as_ref().ok()) {
 							Some(ticket_data) if !ticket_data.is_empty() => {
 								ticket_data.iter().map(|ticket|
 									view! {
@@ -64,6 +65,43 @@ pub fn Dashboard() -> impl IntoView {
 				</tbody>
 			</table>
 		</Transition>
+		<Transition fallback=|| view! { <div class="dashboard_ticket_list_loading">"Loading tickets..."</div> }>
+			{
+				move || match &user_closed_tickets.read().as_ref().and_then(|tickets| tickets.as_ref().ok()) {
+					Some(ticket_data) if !ticket_data.is_empty() => {
+						Some(view! {
+							<table class="dashboard_ticket_list">
+								<thead>
+									<tr>
+										<th>"Ticket"</th>
+										<th>"Closed"</th>
+									</tr>
+								</thead>
+								<tbody>
+									{
+										ticket_data.iter().map(|ticket|
+											view! {
+												<tr>
+													<td>
+														<a href={make_ticket_url(guild_id, &ticket.id)}>
+															{ticket.title.clone()}
+														</a>
+													</td>
+													<td>
+														{ticket.closed_at.to_rfc3339()}
+													</td>
+												</tr>
+											}
+										).collect::<Vec<_>>()
+									}
+								</tbody>
+							</table>
+						})
+					}
+					_ => None
+				}
+			}
+		</Transition>
 	}
 }
 
@@ -74,16 +112,23 @@ fn make_ticket_url(guild_id: Option<u64>, ticket_id: &str) -> String {
 	}
 }
 
-#[derive(Deserialize, Serialize)]
-pub struct TicketMetadata {
+#[derive(Debug, Deserialize, Serialize)]
+pub struct ActiveTicketMetadata {
 	id: String,
 	title: String,
 	last_message_author_name: String,
 	last_message_time: DateTime<Utc>,
 }
 
+#[derive(Debug, Deserialize, Serialize)]
+pub struct ClosedTicketMetadata {
+	id: String,
+	title: String,
+	closed_at: DateTime<Utc>,
+}
+
 #[server]
-async fn get_active_tickets_for_user(guild_id: Option<u64>) -> Result<Vec<TicketMetadata>, ServerFnError> {
+async fn get_active_tickets_for_user(guild_id: Option<u64>) -> Result<Vec<ActiveTicketMetadata>, ServerFnError> {
 	use crate::discord::users::get_member_data;
 	use crate::model::{Ticket, TicketMessage, database_id_from_discord_id};
 	use crate::schema::{ticket_messages, tickets};
@@ -113,10 +158,14 @@ async fn get_active_tickets_for_user(guild_id: Option<u64>) -> Result<Vec<Ticket
 		)
 		.load(&mut db_connection)?;
 
-	let mut tickets: Vec<TicketMetadata> = Vec::with_capacity(user_tickets.len());
+	let mut tickets: Vec<ActiveTicketMetadata> = Vec::with_capacity(user_tickets.len());
 	for ticket in user_tickets {
 		let last_message: TicketMessage = ticket_messages::table
-			.filter(ticket_messages::ticket.eq(&ticket.id).and(ticket_messages::user_message.is_not_null()))
+			.filter(
+				ticket_messages::ticket
+					.eq(&ticket.id)
+					.and(ticket_messages::user_message.is_not_null()),
+			)
 			.order(ticket_messages::send_time.desc())
 			.first(&mut db_connection)?;
 		let author_id = last_message.get_author();
@@ -127,7 +176,7 @@ async fn get_active_tickets_for_user(guild_id: Option<u64>) -> Result<Vec<Ticket
 			Err(_) => format!("<{}>", author_id.get()),
 		};
 
-		let ticket_metadata = TicketMetadata {
+		let ticket_metadata = ActiveTicketMetadata {
 			id: ticket.id,
 			title: ticket.title,
 			last_message_author_name: author_name,
@@ -135,5 +184,48 @@ async fn get_active_tickets_for_user(guild_id: Option<u64>) -> Result<Vec<Ticket
 		};
 		tickets.push(ticket_metadata);
 	}
+	Ok(tickets)
+}
+
+#[server]
+async fn get_closed_tickets_for_user(guild_id: Option<u64>) -> Result<Vec<ClosedTicketMetadata>, ServerFnError> {
+	use crate::model::{Ticket, database_id_from_discord_id};
+	use crate::schema::tickets;
+	use crate::web::pages::server_utils::{get_guild_id_from_request, get_user_id_from_request};
+	use crate::web::state::AppState;
+	use diesel::prelude::*;
+
+	let guild_id = get_guild_id_from_request(guild_id).await?;
+	let user_id = get_user_id_from_request().await?;
+
+	let (Some(guild_id), Some(user_id)) = (guild_id, user_id) else {
+		return Ok(Vec::new());
+	};
+
+	let db_guild_id = database_id_from_discord_id(guild_id.get());
+	let db_user_id = database_id_from_discord_id(user_id.get());
+
+	let state: AppState = expect_context();
+	let mut db_connection = state.db_connection_pool.get()?;
+
+	let user_tickets: Vec<Ticket> = tickets::table
+		.filter(
+			tickets::guild
+				.eq(db_guild_id)
+				.and(tickets::with_user.eq(db_user_id))
+				.and(tickets::closed_at.is_not_null()),
+		)
+		.order(tickets::closed_at.desc())
+		.load(&mut db_connection)?;
+
+	let tickets: Vec<ClosedTicketMetadata> = user_tickets
+		.into_iter()
+		.map(|ticket| ClosedTicketMetadata {
+			id: ticket.id,
+			title: ticket.title,
+			closed_at: ticket.closed_at.unwrap(),
+		})
+		.collect();
+
 	Ok(tickets)
 }
